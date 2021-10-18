ec714d1770047a8af6c741b7b1bed00fa114bf1a
Ishani Saravanan <ishanisaravanan@gmail.com>
2021-10-18 14:13:06 +0530

    Update 136. Single Number.cpp

--- 136. Single Number.cpp
+++ 136. Single Number.cpp
@@ -3,16 +3,6 @@ public:
     int singleNumber(vector<int>& nums)
     {
        int n=nums.size();
-        // unordered_map <int, int> map;
-        // for(int i=0 ;i <n ;i++)
-        // {
-        //     map[nums[i]]++;
-        // }
-        // for(int i=0 ; i<n ; i++)
-        // {
-        //     if(map[nums[i]] == 1)
-        //         return nums[i];
-        // }  
         int ans=0;
          for(int i=0 ; i<n ; i++)
         {
