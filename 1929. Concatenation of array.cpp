5b6f39a18c9851ced294866974b401bf960edaaa
Ishani Saravanan <ishanisaravanan@gmail.com>
2021-10-18 14:08:27 +0530

    1929

---
+++ 1929. Concatenation of array.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+       int n= nums.size();
+        vector<int> ans(2*n , 0);
+        
+        for(int i=0 ; i<n ; i++){
+            ans[i] = nums[i];
+            ans[i+n] = nums[i];
+        }
+        
+        return ans;
+       
+    }
+};
