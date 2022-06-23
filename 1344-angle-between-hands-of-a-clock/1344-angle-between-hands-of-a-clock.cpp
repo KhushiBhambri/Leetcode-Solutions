57f303f7ddc178cd556bca9b6d90ec0b1f1e196a
Khushi <64163023+KhushiBhambri@users.noreply.github.com>
2022-06-23 16:45:59 +0530

    Time: 0 ms (100.00%), Space: 6 MB (32.25%) - LeetHub

--- 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
+++ 1344-angle-between-hands-of-a-clock/1344-angle-between-hands-of-a-clock.cpp
@@ -2,14 +2,12 @@ class Solution {
 public:
     double angleClock(int hour, int minutes) {
         hour= hour%12;
-        double HinH = double(360)/12;
-        double HinM = double(360)/(12*60);
-        double MinM = double(360)/60;
-        // cout<<HinH<<" "<<HinM<<endl;
+        double HinH = double(360)/12;  // degrees moved by hour hand in 1 hour
+        double HinM = double(360)/(12*60);  // deg moved by hour hand in 1 min
+        double MinM = double(360)/60;     // deg moved by min hand in 1 min.
         double H = hour*HinH + HinM*minutes;
         double M = MinM*minutes;
         double angle = abs(M-H);
-        // cout<<H<<" "<<M<<endl;
         return min(angle,360-angle);
     }
 };
