04111ef144b0919d051536eb4e2ee9f0c93e7241
Khushi <64163023+KhushiBhambri@users.noreply.github.com>
2022-06-24 02:08:04 +0530

    Time: 275 ms (81.66%), Space: 56.1 MB (85.88%) - LeetHub

---
+++ 630-course-schedule-iii/630-course-schedule-iii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    static bool comp(vector<int> &a, vector<int> &b){
+        return a[1]<b[1];
+    }
+    int scheduleCourse(vector<vector<int>>& courses) {
+        int n = courses.size();
+        priority_queue<int> pq;
+        // for(int i=0;i<n;i++){
+        //     int d=courses[i][0];
+        //     int l=courses[i][1];
+        //     pq.push({courses[i][0],courses[i][1]});
+        // }
+        int last=0;
+        int ans=0;
+        sort(courses.begin(),courses.end(),comp);
+        for(int i=0;i<n;i++){
+            if(last+courses[i][0]<=courses[i][1]) {
+                ans++;
+                last+= courses[i][0];
+                pq.push(courses[i][0]);
+            }
+            else{
+                if(pq.size() && pq.top()>courses[i][0]){
+                    last+=courses[i][0]-pq.top();
+                    pq.pop();
+                    pq.push(courses[i][0]);
+                }
+            }
+        }
+        return ans;
+    }
+};
