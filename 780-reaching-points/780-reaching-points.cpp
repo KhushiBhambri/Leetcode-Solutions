8bcb19bb7c8e6c54b09899a06fc244cd69989ad3
Khushi <64163023+KhushiBhambri@users.noreply.github.com>
2022-06-25 00:12:28 +0530

    Time: 9 ms (6.47%), Space: 5.8 MB (61.81%) - LeetHub

---
+++ 780-reaching-points/780-reaching-points.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while(sx<=tx && sy<=ty){
+            if(sx==tx && sy==ty) return true;
+            cout<<tx<<" "<<ty<<endl;
+            if(tx>ty){
+                if(sy == ty) return (tx - sx) % ty == 0;
+                tx=tx%ty;
+            }
+            else{
+                if(sx == tx) return (ty - sy) % tx == 0;
+                ty=ty%tx;
+            }
+        }
+        return false;
+    }
+};
